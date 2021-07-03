61a4f27b5439273a86baf31bfb3a35202e87fa1a
Oleg Butuzov <butuzov@users.noreply.github.com>
2021-07-03 07:36:09 +0300

    Update errorf.go

--- errorf.go
+++ errorf.go
@@ -1,9 +1,31 @@
 package errorf
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"bytes"
+)
 
 type T struct{}
 
 func (t *T) Errorf(format string, args ...interface{}) {
-	println(fmt.Errorf(format, args...).Error())
+	err := fmt.Errorf(format, args...)
+	
+	if err.Error() == "" {
+		return
+	}
+	
+	var message bytes.Buffer
+	for _, line := range strings.Split(err.Error(), "\n") {
+	    line = strings.TrimLeft(line, "\t")
+	    if strings.HasPrefix(line, "Error:") || message.Len() > 0 {
+		line = strings.ReplaceAll(line, "Error:", "\t")
+		line = strings.TrimLeft(line, "            	")
+		message.WriteString(line)
+		message.WriteByte('\n')
+	    }   
+	}
+ 
+	fmt.Fprint(os.Stderr, strings.TrimRight(message.String(), "\n")) 
 }
